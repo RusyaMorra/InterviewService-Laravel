ba35337da77d2066b4f6d27db3ddeabb61207c1d
Rusya Morra <80226063+RusyaMorra@users.noreply.github.com>
2022-07-16 12:24:05 +0600

    frontAdded

---
+++ ServiceFront/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'; //Импортим сам реакт
+import ReactDOM from 'react-dom';  //Подключаем библиотеку ReactDOM
+
+import './scss/index.css';
+import App from './App'; // Подключаем входной компоннент app
+
+import reportWebVitals from './reportWebVitals';// определяет ошибки в приложении
+
+ReactDOM.render( // Рендерим входной компоннент с помощью  ReactDOM
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+reportWebVitals();
